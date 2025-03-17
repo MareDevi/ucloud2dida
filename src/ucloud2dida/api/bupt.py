4e4f39456a6ea9d507e951e3859ebf95d4b697ca
maredevi <maredevi@outlook.com>
2025-03-17 17:20:36 +0800

    change api source

--- src/ucloud2dida/api/bupt.py
+++ src/ucloud2dida/api/bupt.py
@@ -1,7 +1,6 @@
 from ..utils.logger import logger
-from ..auth.bupt_auth import get_co_and_sa
+from requests.auth import HTTPBasicAuth
 import os
-import json
 import requests
 from dotenv import load_dotenv
 
@@ -11,44 +10,16 @@ class BuptAPI:
         load_dotenv()
         self.account = os.getenv("BUPT_ACCOUNT")
         self.password = os.getenv("BUPT_PASSWORD")
-        self._setup_endpoints()
-
-    def _setup_endpoints(self):
-        """设置API端点"""
-        self.base_url = "https://apiucloud.bupt.edu.cn"
-        self.file_url = "https://fileucloud.bupt.edu.cn/ucloud/document/"
-        self.assignment_file_url = f"{self.base_url}/blade-source/resource/filePath"
-
-    def _refresh_auth(self):
-        """刷新认证信息"""
-        logger.info("刷新北邮认证信息")
-        get_co_and_sa(self.account, self.password)
-
-        with open("auth.json", "r") as file:
-            account_data = json.load(file)
-            self.tenant_id = account_data["tenant_id"]
-            self.user_id = account_data["user_id"]
-            self.auth_token = account_data["auth_token"]
-            self.blade = account_data["blade"]
-
-        self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
-            "Accept": "application/json, text/plain, */*",
-            "Authorization": self.auth_token,
-            "Tenant-Id": self.tenant_id,
-            "Blade-Auth": self.blade,
-        }
+        self.url = "https://ucloud.maredevi.workers.dev/"
 
     def get_todo_list(self):
         logger.info("正在获取待办事项列表")
-        self._refresh_auth()
         response = requests.get(
-            f"{self.base_url}/ykt-site/site/student/undone",
-            headers=self.headers,
-            params={"userId": self.user_id},
+            f"{self.url}/undoneList",
+            auth=HTTPBasicAuth(self.account, self.password)
         )
         data = response.json()
-        todos = data["data"]["undoneList"]
+        todos = data["undoneList"]
         logger.info(f"成功获取到 {len(todos)} 个待办事项")
         for todo in todos:
             yield todo
@@ -57,11 +28,12 @@ class BuptAPI:
         """获取作业详情"""
         logger.info(f"正在获取作业 {assignment_id} 的详情")
         response = requests.get(
-            f"{self.base_url}/ykt-site/work/detail",
-            headers=self.headers,
-            params={"assignmentId": assignment_id},
+            f"{self.url}/homework",
+            params={"id": assignment_id},
+            auth=HTTPBasicAuth(self.account, self.password)
         )
-        data = response.json()["data"]
+
+        data = response.json()
         return {
             "title": data["assignmentTitle"],
             "content": data["assignmentContent"],
--- src/ucloud2dida/auth/__init__.py
+++
@@ -1,3 +0,0 @@
-from .bupt_auth import get_co_and_sa
-
-__all__ = ["get_co_and_sa"]
